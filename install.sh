4f4bf33a264acc3bd49715ad77c708761f527200
denosawr <denosawr@vivaldi.net>
2018-10-27 21:17:21 +0800

    Install.sh changes.

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ fi
 echo "Installing Python, Pip and sed, if not already installed..."
 sudo apt-get install python python-pip -y > /dev/null
 
-echo "Cloning MCPi-Scratch to folder from https://github.com/denosawr/MCPi-scratch..."
+echo "Cloning MCPi-Scratch to folder from https://github.com/denosawr/mcpi-scratch..."
 git clone --quiet https://github.com/denosawr/MCPi-Scratch.git $INSTALLDIR --branch $BRANCH
 cd $INSTALLDIR
 
@@ -36,8 +36,8 @@ sudo cp -f mcpi-scratch/mcpi-scratch.js /usr/lib/scratch2/scratch_extensions
 echo "Installing desktop executable..."
 chmod +x mcpi-scratch/mcpi-scratch-server.py
 sed -i -e "s/Icon=.*/Icon=$(realpath installfiles/mcpi-scratch.png | sed 's_/_\\/_g')/" installfiles/mcpi-scratch-server.desktop
-sed -i -e "s/Exec=.*/Exec=$(realpath mcpi-scratch/mcpi-scratch-server.py | sed 's_/_\\/_g')/" installfiles/mcpi-scratch-server.desktop
-cp installfiles/mcpi-scratch-server.desktop ~/.local/share/applications/
-cp installfiles/mcpi-scratch-server.desktop ~/Desktop
+sed -i -e "s/Exec=.*/Exec=lxterminal --command=\"\/bin\/bash -c 'python -i $(realpath mcpi-scratch/mcpi-scratch-server.py | sed 's_/_\\/_g')'\"/" installfiles/mcpi-scratch-server.desktop
+cp -f installfiles/mcpi-scratch-server.desktop ~/.local/share/applications/
+cp -f installfiles/mcpi-scratch-server.desktop ~/Desktop
 
 echo "Complete! Use the launcher to get started."
